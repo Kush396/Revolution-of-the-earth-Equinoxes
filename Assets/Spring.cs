6e784992066fc63617281c4a6e364f43e4e70b9a
Kushal Bansal <bansalkushal396@gmail.com>
2023-07-21 16:06:18 +0530

    add game files

---
+++ Assets/Spring.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spring : MonoBehaviour
+{
+    public void PlayGame3()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    // public void PlayGame4()
+    // {
+    //     SceneManager.LoadSceneAsync(4);
+    // }
+}
